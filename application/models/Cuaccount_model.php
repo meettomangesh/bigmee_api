476a6d65f6471888954edc4ad97517fd08ce94a6
unknown <meettomangesh@gmail.com>
2018-04-15 13:08:07 +0530

    to save work

--- application/models/Cuaccount_model.php
+++ application/models/Cuaccount_model.php
@@ -1,11 +1,11 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Cuaccount_model extends MY_model {
-    
-    public function __construct() {
-        parent::__construct();
-        
-    }
-    
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cuaccount_model extends MY_model {
+    
+    public function __construct() {
+        parent::__construct();
+        
+    }
+    
 }
