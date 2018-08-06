3af0e5855c509f7bbe0f63d5c59e435439fc1fc3
mangeshn <mangeshn@iprogrammer.com>
2018-08-06 11:17:13 +0530

    To save work

--- application/controllers/app/TelecomApi.php
+++ application/controllers/app/TelecomApi.php
@@ -33,53 +33,41 @@ class TelecomApi extends BASE_Api {
 
     public function teleservices_post() {
         $serviceData = $this->telecomserviceapi_model->get_data_by('telecom_services', array('status' => '1'), array('id', 'service_name', 'status', 'service_logo'), array('sequence', 'asc'));
-        $serviceId = 3;
         //$api = $this->supplier_model->get_row_byid('telecom_api', array('status' => 1), array('api_id'));
         //$api = $this->supplier_model->get_row_byid('sms_api', array('api_status' => 1,'type' => 'telecom'), array('api_id'));
         $api = $this->telecomserviceapi_model->get_data_by('sms_api', array('type' => 'telecom', 'api_status' => 1), array('api_id'));
         $apiId = array_column($api, 'api_id');
 
-        // foreach ($serviceData as $sKey => $serviceRow) {
-        $condition = 'service_id=' . $serviceId . ' AND status="1" AND (api_id = "3" OR api_id = "5")';
-        $providerData = $this->telecomserviceapi_model->get_tele_data_by('telecom_providers', $condition, array('id', 'api_id', 'provider_name', 'provider_code', 'status', 'provider_logo'), array('provider_name', 'asc'));
-        $serviceData['providers'] = $providerData;
-        //  }
-        echo '<pre>';
-        print_r($providerData);
-        exit;
+        foreach ($serviceData as $sKey => $serviceRow) {
+            //$providerData = $this->supplier_model->get_data_by('telecom_providers', array('service_id' => $serviceRow['id'], 'status' => '1','api_id' => $api->api_id), array('id', 'provider_name', 'provider_code', 'status', 'provider_logo'), array('provider_name', 'asc'));
+            $condition = 'service_id=' . $serviceRow['id'] . ' AND status="1" AND (api_id = "3" OR api_id = "5")';
+            $providerData = $this->telecomserviceapi_model->get_tele_data_by('telecom_providers', $condition, array('id', 'api_id', 'provider_name', 'provider_code', 'status', 'provider_logo'), array('provider_name', 'asc'));
+            $serviceData[$sKey]['providers'] = $providerData;
+        }
         $response['status'] = 'OK';
         $response['data'] = $serviceData;
         die(json_encode($response));
     }
-    
-    /**
-     * To get all available providers by specific service id
-     */
-    
-    public function getAllProvidersByServiceId_post() {
+
+    public function getProviderDataByServiceId_post() {
         $serviceId = $this->post('service_id');
-
-        if (empty($serviceId)) {
-            $this->response(array('status' => REST_Controller::HTTP_BAD_REQUEST, 'data' => 'Provide Service Id.'), REST_Controller::HTTP_BAD_REQUEST);
+        if(empty($serviceId) || $serviceId == '') {
+            $this->response(array('status' => REST_Controller::HTTP_BAD_REQUEST, 'data' => 'Please provide service id.'));
         }
-
-        $serviceData = $this->telecomserviceapi_model->get_data_by('telecom_services', array('status' => '1', 'id' => $serviceId), array('id', 'service_name', 'status'), array('sequence', 'asc'));
-        if (empty($serviceData)) {
-            $this->response(array('status' => REST_Controller::HTTP_BAD_REQUEST, 'data' => 'Please provider valid service id.'), REST_Controller::HTTP_BAD_REQUEST);
-        }
-
         $condition = 'service_id=' . $serviceId . ' AND status="1" AND (api_id = "3" OR api_id = "5")';
         $providerData = $this->telecomserviceapi_model->get_tele_data_by('telecom_providers', $condition, array('id', 'api_id', 'provider_name', 'provider_code', 'status', 'provider_logo'), array('provider_name', 'asc'));
-       
+        if(empty($providerData)){
+           $this->response(array('status' => REST_Controller::HTTP_BAD_REQUEST, 'data' => 'Please provide valid service id.')); 
+        }
         foreach ($providerData as $sKey => $providerRow) {
-            $providerData[$sKey]['plan'] = $this->telecomserviceapi_model->getPlanDataByProviderId($providerRow['id']);
-            
+            //$providerData = $this->supplier_model->get_data_by('telecom_providers', array('service_id' => $serviceRow['id'], 'status' => '1','api_id' => $api->api_id), array('id', 'provider_name', 'provider_code', 'status', 'provider_logo'), array('provider_name', 'asc'));
+            $planData = $this->telecomserviceapi_model->getPlanDataByProviderId($providerRow['id']);
+            $providerData[$sKey]['plan'] = $planData;
+            $providerData[$sKey]['formElement'] = $this->telecomserviceapi_model->getFormFieldsByServiceId($serviceId);
+            $providerData[$sKey]['otherFormElement'] = $this->telecomserviceapi_model->loadOtherParamByServiceId($serviceId);
         }
-        if (empty($providerData)) {
-            $this->response(array('status' => REST_Controller::HTTP_BAD_REQUEST, 'data' => 'Providers not available to this service.'), REST_Controller::HTTP_BAD_REQUEST);
-        } else {
-            $this->response(array('status' => REST_Controller::HTTP_OK, 'data' => $providerData), REST_Controller::HTTP_OK);
-        }
+        
+        $this->response(array('status' => REST_CONTROLLER::HTTP_OK, 'data' => $providerData));
     }
 
 }
--- application/models/Telecomserviceapi_model.php
+++ application/models/Telecomserviceapi_model.php
@@ -1,1 +1,109 @@
-<?phpdefined('BASEPATH') OR exit('No direct script access allowed');class Telecomserviceapi_model extends MY_model {    private $table = 'telecom_services';    public function __construct() {        parent::__construct();    }    /**     * To get all available plans for specific providers     * @param type $providerId     */    public function getPlanDataByProviderId($providerId) {        $sql = "SELECT ts.scheme_name,ts.scheme_id,tp.*                FROM `telecom_plan` AS tp                 JOIN `telecom_scheme` AS ts ON tp.scheme_id = ts.scheme_id                 WHERE provider_id = ?";        return $this->db->query($sql, array($providerId))->result();    }}
+<?php
+
+ defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Telecomserviceapi_model extends MY_model {
+
+    private $table = 'telecom_services';
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+    /**
+     * To get all active plans against provider id
+     * @param type $provider_id
+     */
+    public function getPlanDataByProviderId($provider_id) {
+        $getPlanByProviderIdQuery = "SELECT
+                    ts.scheme_name,
+                    tp.*
+                FROM
+                    `telecom_plan` AS tp
+                JOIN `telecom_scheme` AS ts
+                ON
+                    tp.scheme_id = ts.scheme_id
+                WHERE
+                    tp.provider_id = ?";
+        return $this->db->query($getPlanByProviderIdQuery, array($provider_id))->result();
+    }
+
+    //load all billing units
+    public function loadOtherParamByServiceId($service_id) {
+        $otherParamData = [];
+        switch ($service_id) {
+            case 6:
+                $billing_unit_data['billing_units'] = $this->getBilliingUnits();
+                $processing_cycle_data['processing_cycle'] = array(
+                    '01' => '01',
+                    '02' => '02',
+                    '03' => '03',
+                    '04' => '04',
+                    '05' => '05',
+                    '06' => '06',
+                    '07' => '07',
+                    '08' => '08'
+                );
+                $otherParamData = array_merge($otherParamData,$billing_unit_data,$processing_cycle_data);
+                break;
+            default:
+                break;
+        }
+        return $otherParamData;
+    }
+
+    public function getBilliingUnits() {
+        $this->db->select("unit_name")
+                ->from("billing_unit");
+        return $this->db->get()->result();
+        // echo $this->db->get_compiled_select();	
+    }
+
+    /**     *
+     * To get all required form elements by service Id
+     */
+    public function getFormFieldsByServiceId($service_id) {
+        $fieldsArr = array('service_id', 'provider_id');
+        Switch ($service_id) {
+            case 3: // For prepaid
+                $fieldsArr = array_merge($fieldsArr, array('mobile_number', 'amount'));
+                break;
+            case 12: // For postpaid
+                $fieldsArr = array_merge($fieldsArr, array('mobile_number', 'amount'));
+                break;
+            case 4: // For DTH
+                $fieldsArr = array_merge($fieldsArr, array('customer_id', 'amount'));
+                break;
+            case 5: // For LandLine
+                $fieldsArr = array_merge($fieldsArr, array('landline_no', 'account_number', 'amount'));
+                break;
+            case 9: // For Gas Line
+                $fieldsArr = array_merge($fieldsArr, array('account_number', 'amount'));
+                break;
+            case 13: // For Data Card
+                $fieldsArr = array_merge($fieldsArr, array('number', 'amount'));
+                break;
+            case 7: // For INSURANCE
+                $fieldsArr = array_merge($fieldsArr, array('policy_number', 'amount'));
+                break;
+            case 11: // For UTILITY BILL
+                $fieldsArr = array_merge($fieldsArr, array('amount'));
+                break;
+            case 6: // For ELECTRICITY
+                $fieldsArr = array_merge($fieldsArr, array('billing_unit', 'processing_cycle', 'mobile_number', 'consumer_number', 'amount'));
+                break;
+            case 8: // For MONEY
+                $fieldsArr = array_merge($fieldsArr, array('customer_id', 'amount'));
+                break;
+            case 10: // For LOAN EMI
+                $fieldsArr = array_merge($fieldsArr, array('customer_id', 'amount'));
+                break;
+            case 14: // For PANCARD
+                $fieldsArr = array_merge($fieldsArr, array('customer_id', 'amount'));
+                break;
+        }
+        return $fieldsArr;
+    }
+
+}
